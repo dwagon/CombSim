3a1040b019229dc693a6771a1f4558f6610be790
Dougal Scott <dougal.scott@gmail.com>
2023-05-14 16:01:25 +1000

    Take Wizard to level 3

--- Characters/Wizard.cs
+++ Characters/Wizard.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using CombSim.Spells;
 
 namespace CombSim.Characters
 {
@@ -7,13 +8,16 @@ namespace CombSim.Characters
     {
         private readonly Dictionary<int, int> _hitPointsAtLevel = new Dictionary<int, int>()
         {
-            { 1, 8 }, { 2, 12 }
+            { 1, 8 }, { 2, 14 }, {3, 20}
         };
         
         // CasterLevel: <SpellLevel: NumberOfSlots>
         private readonly Dictionary<int, Dictionary<int, int>> _spellsAtLevel = new Dictionary<int, Dictionary<int, int>>()
         {
             {1, new Dictionary<int, int>() {{1, 2}}},
+            {2, new Dictionary<int, int>() {{1, 3}}},
+            {3, new Dictionary<int, int>() {{1, 4}, {2, 2}}},
+
         };
 
         public Wizard(string name, int level = 1, string team = "Wizards") : base(name, team)
@@ -33,8 +37,12 @@ namespace CombSim.Characters
             AddEquipment(Gear.Quarterstaff);
             // AddEquipment(Gear.LightCrossbow);
 
-            AddSpell(new Spells.RayOfFrost());
-            AddSpell(new Spells.MagicMissile());
+            AddSpell(new RayOfFrost());
+            AddSpell(new MagicMissile());
+            if (Level >= 3)
+            {
+                AddSpell(new ScorchingRay());
+            }
         }
 
         public override string ToString()
@@ -50,7 +58,6 @@ namespace CombSim.Characters
 
         public override bool CanCastSpell(Spell spell)
         {
-            Console.WriteLine($"// CanCastSpell(spell={spell.Name()})");
             if (spell.Level == 0) return true;
             if (_spellsAtLevel[Level][spell.Level] >= 1) return true;
             return false;
@@ -58,6 +65,7 @@ namespace CombSim.Characters
 
         public override void DoCastSpell(Spell spell)
         {
+            Console.WriteLine($"// DoCastSpell(spell={spell.Name()}) {spell.Level}");
             if (spell.Level == 0) return;
             _spellsAtLevel[Level][spell.Level]--;
         }
