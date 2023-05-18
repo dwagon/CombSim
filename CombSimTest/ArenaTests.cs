63974a54fa168dba932850000a355cc5cd19b62b
Dougal Scott <dougal.scott@gmail.com>
2023-05-18 18:49:55 +1000

    Start Testing

---
+++ CombSimTest/ArenaTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using CombSim;
+
+namespace CombSimTest
+{
+    [TestFixture]
+    public class ArenaTests
+    {
+        [Test]
+        public void TestArenaCreation()
+        {
+            Arena a = new Arena(10,10);
+            Assert.True( a.IsClear(new Location(5,5)));
+        }
+
+        [Test]
+        public void TestNeighbours()
+        {
+            Arena a = new Arena(10, 10);
+            var TLneighbours = a.GetNeighbours(new Location(0, 0));
+            Assert.AreEqual(TLneighbours.Count, 3);
+            Assert.AreEqual(TLneighbours, new List<Location> {new Location(0,1), new Location(1,0), new Location(1,1)});
+            
+            var BRneighbours = a.GetNeighbours(new Location(9, 9));
+            Assert.AreEqual(BRneighbours.Count, 3);
+            Assert.AreEqual(BRneighbours, new List<Location> {new Location(8,8), new Location(8,9), new Location(9,8)});
+            
+            var Cneighbours = a.GetNeighbours(new Location(5, 5));
+            Assert.AreEqual(Cneighbours.Count, 8);
+            Assert.AreEqual(Cneighbours, new List<Location>
+            {
+                new Location(4,4), new Location(4,5), new Location(4,6), 
+                new Location(5,4), new Location(5,6),
+                new Location(6,4), new Location(6,5), new Location(6,6)
+            });
+            
+        }
+    }
+}
---
+++ CombSimTest/LocationTests.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+using CombSim;
+
+namespace CombSimTest
+{
+    [TestFixture]
+    public class LocationTests
+    {
+        [Test]
+        public void TestLocationCreation()
+        {
+            var location = new Location(5, 5);
+            Assert.AreEqual(location.X, 5);
+            Assert.AreEqual(location.Y, 5);
+        }
+
+        [Test]
+        public void TestEquals()
+        {
+            var location = new Location(3, 4);
+            Assert.AreEqual(location, new Location(3,4));
+        }
+
+        [Test]
+        public void TestToString()
+        {
+            var location = new Location(3, 4);
+            Assert.AreEqual(location.ToString(), "(3,4)");
+        }
+
+        [Test]
+        public void TestDistance()
+        {
+            var locationA = new Location(5, 5);
+            var locationB = new Location(5, 7);
+            Assert.AreEqual(locationA.DistanceBetween(locationB), 2f);
+        }
+
+        [Test]
+        public void TestDblEquals()
+        {
+            var locationA = new Location(5, 5);
+            var locationB = new Location(5, 5);
+            Assert.True(locationA == locationB);
+        }
+    }
+}
