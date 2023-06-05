1cfe7a5ebe3bfd2e69428c8c8e4da70c641329ef
Dougal Scott <dougal.scott@gmail.com>
2023-06-06 09:43:07 +1000

    Set Finesse weapons

--- CombSim/Gear/MeleeWeaponGear.cs
+++ CombSim/Gear/MeleeWeaponGear.cs
@@ -3,37 +3,36 @@ namespace CombSim.Gear
     public static class MeleeWeaponGear
     {
         public static readonly MeleeWeapon Longsword =
-            new MeleeWeapon("Longsword", new DamageRoll("1d8", 0, DamageTypeEnums.Piercing), 5 / 5);
+            new MeleeWeapon("Longsword", new DamageRoll("1d8", DamageTypeEnums.Piercing));
 
         public static readonly MeleeWeapon Greatclub =
-            new MeleeWeapon("Greatclub", new DamageRoll("1d8", 0, DamageTypeEnums.Bludgeoning), 5 / 5);
+            new MeleeWeapon("Greatclub", new DamageRoll("1d8", DamageTypeEnums.Bludgeoning));
 
         public static readonly MeleeWeapon Dagger =
-            new MeleeWeapon("Dagger", new DamageRoll("1d4", 0, DamageTypeEnums.Piercing), 5 / 5);
+            new MeleeWeapon("Dagger", new DamageRoll("1d4", DamageTypeEnums.Piercing), finesse: true);
 
         public static readonly MeleeWeapon Mace =
-            new MeleeWeapon("Mace", new DamageRoll("1d6", 0, DamageTypeEnums.Bludgeoning), 5 / 5);
+            new MeleeWeapon("Mace", new DamageRoll("1d6", DamageTypeEnums.Bludgeoning));
 
         public static readonly MeleeWeapon MacePlusOne =
-            new MeleeWeapon("Mace +1", new DamageRoll("1d6", 0, DamageTypeEnums.MagicBludgeoning), bonus: 1,
-                reach: 5 / 5);
+            new MeleeWeapon("Mace +1", new DamageRoll("1d6", DamageTypeEnums.MagicBludgeoning), bonus: 1);
 
         public static readonly MeleeWeapon Flail =
-            new MeleeWeapon("Flail", new DamageRoll("1d8", 0, DamageTypeEnums.Bludgeoning), 5 / 5);
+            new MeleeWeapon("Flail", new DamageRoll("1d8", DamageTypeEnums.Bludgeoning));
 
         public static readonly MeleeWeapon Scimitar =
-            new MeleeWeapon("Scimitar", new DamageRoll("1d6", 0, DamageTypeEnums.Slashing), 5 / 5);
+            new MeleeWeapon("Scimitar", new DamageRoll("1d6", DamageTypeEnums.Slashing), finesse: true);
 
         public static readonly MeleeWeapon Shortsword =
-            new MeleeWeapon("Shortsword", new DamageRoll("1d6", 0, DamageTypeEnums.Piercing), 5 / 5);
+            new MeleeWeapon("Shortsword", new DamageRoll("1d6", DamageTypeEnums.Piercing));
 
         public static readonly MeleeWeapon Quarterstaff =
-            new MeleeWeapon("Quarterstaff", new DamageRoll("1d6", 0, DamageTypeEnums.Bludgeoning), 5 / 5);
+            new MeleeWeapon("Quarterstaff", new DamageRoll("1d6", DamageTypeEnums.Bludgeoning));
 
         public static readonly MeleeWeapon Spear =
-            new MeleeWeapon("Spear", new DamageRoll("1d6", 0, DamageTypeEnums.Piercing), 5 / 5);
+            new MeleeWeapon("Spear", new DamageRoll("1d6", DamageTypeEnums.Piercing));
 
         public static readonly MeleeWeapon Morningstar =
-            new MeleeWeapon("Morningstar", new DamageRoll("1d8", 0, DamageTypeEnums.Piercing), 5 / 5);
+            new MeleeWeapon("Morningstar", new DamageRoll("1d8", DamageTypeEnums.Piercing));
     }
 }
