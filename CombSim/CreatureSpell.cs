64d2d03846908cbaeadd9dd8527e51bffc5df78a
Dougal Scott <dougal.scott@gmail.com>
2023-06-14 14:44:26 +1000

    Concentration spell

--- CombSim/CreatureSpell.cs
+++ CombSim/CreatureSpell.cs
@@ -1,12 +1,41 @@
 using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CombSimTest")]
 
 namespace CombSim
 {
     public partial class Creature
     {
+        internal Spell _concentration; // What spell the creature is concentrating on if any
+
         public virtual bool CanCastSpell(Spell spell)
         {
             throw new NotImplementedException();
+        }
+
+        public Spell ConcentratingOn()
+        {
+            return _concentration;
+        }
+
+        public void ConcentrateOn(Spell spell)
+        {
+            _concentration = spell;
+        }
+
+        public void MakeConcentrationCheck(object sender, OnTakingDamageEventArgs e)
+        {
+            var diff = Math.Max(10, e.Damage.hits / 2);
+            if (MakeSavingThrow(StatEnum.Constitution, diff, out var roll))
+            {
+                Console.WriteLine($"Succeeded Concentration check ({roll} vs DC {diff})");
+                return;
+            }
+
+            Console.WriteLine($"Failed Concentration check ({roll} vs DC {diff}) - {_concentration.Name()} ending");
+            _concentration?.EndConcentration();
+            _concentration = null;
         }
 
         public virtual void DoCastSpell(Spell spell)
@@ -14,6 +43,7 @@ namespace CombSim
             throw new NotImplementedException();
         }
 
+        // Add spell to a character so that they can cast it
         protected void AddSpell(Spell spell)
         {
             _spells[spell.Name()] = spell;
---
+++ CombSimTest/SpellConcentrationTest.cs
@@ -0,0 +1,47 @@
+using CombSim;
+using NUnit.Framework;
+
+namespace CombSimTest
+{
+    [TestFixture]
+    public class SpellConcentrationTest
+    {
+        [Test]
+        public void TestCasting()
+        {
+            var caster = new TestCaster();
+            var action = caster.PickActionByName("Test Spell");
+            Assert.AreEqual(null, caster._concentration);
+            action.Perform(caster);
+            Assert.AreEqual("Test Spell", caster._concentration.Name());
+        }
+
+        [Test]
+        public void IsConcentrationTest()
+        {
+            var spell = new TestSpell();
+            Assert.True(spell.IsConcentration());
+        }
+    }
+
+    public class TestCaster : Character
+    {
+        public TestCaster() : base("Test Caster", 1)
+        {
+            AddSpell(new TestSpell());
+        }
+
+        public override bool CanCastSpell(Spell spell)
+        {
+            return true;
+        }
+    }
+
+    public class TestSpell : Spell
+    {
+        public TestSpell() : base("Test Spell", 0, ActionCategory.Bonus)
+        {
+            Concentration = true;
+        }
+    }
+}
