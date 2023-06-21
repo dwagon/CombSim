8243f5cd0986b0eae1654147a2e04a02a8110543
Dougal Scott <dougal.scott@gmail.com>
2023-06-21 10:10:27 +1000

    Spirit Guardians do damage to those in the effect area

--- CombSim/Spells/SpiritGuardians.cs
+++ CombSim/Spells/SpiritGuardians.cs
@@ -53,6 +53,7 @@ namespace CombSim.Spells
             foreach (var creature in target.Game.GetAllEnemies(target))
             {
                 creature.OnMoving += OnCreatureMoving;
+                creature.OnTurnStart += OnCreatureStarting;
                 if (creature.DistanceTo(target) <= MaxRange)
                 {
                     SpiritGuardiansAttack(creature);
@@ -62,20 +63,21 @@ namespace CombSim.Spells
 
         public override void End(Creature target)
         {
+            Console.WriteLine($"// Spirit Guardians ended");
             _caster = target;
             foreach (var creature in target.Game.GetAllEnemies(target))
             {
                 creature.OnMoving -= OnCreatureMoving;
+                creature.OnTurnStart -= OnCreatureStarting;
             }
         }
 
         private void SpiritGuardiansAttack(Creature target)
         {
-            var attackMessage = new AttackMessage(attacker: target.Name, victim: target.Name, attackName: Name);
-            Console.WriteLine("XXX");
+            var attackMessage = new AttackMessage(attacker: _caster.Name, victim: target.Name, attackName: Name);
             target.OnDcAttacked?.Invoke(this, new Creature.OnDcAttackedEventArgs
             {
-                Source = target,
+                Source = _caster,
                 DcSaveStat = StatEnum.Wisdom,
                 DcSaveDc = target.SpellSaveDc(),
                 DmgRoll = _dmgRoll,
@@ -85,6 +87,17 @@ namespace CombSim.Spells
             });
         }
 
+        // Creatures starting their turn in side the guardians
+        private void OnCreatureStarting(object sender, Creature.OnTurnStartEventArgs e)
+        {
+            Creature target = e.Creature;
+            if (target.DistanceTo(_caster) <= MaxRange)
+            {
+                SpiritGuardiansAttack(target);
+            }
+        }
+
+        // Creatures entering the guardians this turn
         private void OnCreatureMoving(object sender, Creature.OnMovingEventArgs e)
         {
             Creature target = e.mover;
