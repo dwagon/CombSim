cf89b238e773a25eb1f4750d0878ebf3cd06e99a
Dougal Scott <dougal.scott@gmail.com>
2023-07-01 11:05:54 +1000

    Bard Character

--- CombSim/Program.cs
+++ CombSim/Program.cs
@@ -45,6 +45,7 @@ namespace CombSim
             _game.Add_Creature(new Warlock("Warlock", level: 5, humans));
             _game.Add_Creature(new Rogue("Rogue", level: 5, humans));
             _game.Add_Creature(new Cleric("Cleric", level: 5, humans));
+            _game.Add_Creature(new Bard("Bard", 1, humans));
         }
     }
 }
