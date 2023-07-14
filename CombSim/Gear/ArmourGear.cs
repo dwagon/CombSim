cf53054d374970486644edc368a7c57c3b0baf08
Dougal Scott <dougal.scott@gmail.com>
2023-07-14 11:14:05 +1000

    Fighter up to level 6

--- CombSim/Gear/ArmourGear.cs
+++ CombSim/Gear/ArmourGear.cs
@@ -19,6 +19,7 @@ namespace CombSim.Gear
         public static readonly Armour Ring = new Armour("Ring", 14, dexBonus: false);
         public static readonly Armour Plate = new Armour("Plate", 18, dexBonus: false);
         public static readonly Armour PlatePlusOne = new Armour("Plate +1", 18, dexBonus: false, magicBonus: 1);
+        public static readonly Armour PlatePlusTwo = new Armour("Plate +1", 18, dexBonus: false, magicBonus: 2);
 
         // Shields
         public static readonly Armour Shield = new Armour("Shield", armourClassBonus: 2);
