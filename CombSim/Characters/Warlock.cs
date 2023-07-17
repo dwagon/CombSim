93efc5f5458371772840312b28e48b8f5e7002c9
Dougal Scott <dougal.scott@gmail.com>
2023-07-17 12:37:08 +1000

    Warlock level 6

--- CombSim/Characters/Warlock.cs
+++ CombSim/Characters/Warlock.cs
@@ -4,13 +4,17 @@ using CombSim.Gear;
 using CombSim.Spells;
 using Pastel;
 
+// TODO: Dark Ones Blessing
+// TODO: Casting spells at higher level
+// TODO: Dark Ones Own Luck
+
 namespace CombSim.Characters
 {
     public class Warlock : Character
     {
         private readonly Dictionary<int, int> _hitPointsAtLevel = new Dictionary<int, int>
         {
-            { 1, 10 }, { 2, 18 }, { 3, 24 }, { 4, 31 }, { 5, 38 }
+            { 1, 10 }, { 2, 18 }, { 3, 24 }, { 4, 31 }, { 5, 38 }, { 6, 45 }
         };
 
         // CasterLevel: <SpellLevel: NumberOfSlots>
@@ -22,6 +26,7 @@ namespace CombSim.Characters
                 { 3, 2 },
                 { 4, 2 },
                 { 5, 2 },
+                { 6, 2 }
             };
 
         public Warlock(string name, int level = 1, string team = "Warlocks") : base(name, level, team)
@@ -40,7 +45,13 @@ namespace CombSim.Characters
             Stats.Add(StatEnum.Charisma, new Stat(15));
 
             AddEquipment(new HealingPotion());
-            AddEquipment(MeleeWeaponGear.Quarterstaff);
+
+            // Equipment
+            if (level >= 6) AddEquipment(MeleeWeaponGear.QuarterstaffPlusOne);
+            else AddEquipment(MeleeWeaponGear.Quarterstaff);
+
+            if (level >= 3) AddEquipment(ArmourGear.StuddedLeatherPlusOne);
+            else AddEquipment(ArmourGear.StuddedLeather);
 
             // Cantrips
             AddSpell(new Thunderclap());
@@ -62,13 +73,7 @@ namespace CombSim.Characters
                 AddSpell(new FireBolt());
                 AddSpell(new ShockingGrasp());
                 // AddSpell(new ViciousMockery());
-                AddEquipment(ArmourGear.StuddedLeatherPlusOne);
             }
-            else
-            {
-                AddEquipment(ArmourGear.StuddedLeather);
-            }
-
 
             if (level >= 4)
             {
